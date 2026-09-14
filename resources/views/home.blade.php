9cc92de23e4ce240c9d57750582bc381e53360a3
BaderHalimi <bader.halimi.2003@gmail.com>
2026-09-14 12:51:43 +0300

    Adjust header English title spacing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -814,6 +814,7 @@
         font-family: var(--font-mono);
         font-size: 10.5px;
         letter-spacing: 0.14em;
+        margin-block-start: 3px;
         color: var(--neon-primary);
         text-transform: uppercase;
     }
